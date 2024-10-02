ad341f10b64c6422812645474b27473992603301
Jeanette Mikkelsen <jeanette.mikkelsen@utb.ecutbildning.se>
2024-10-02 19:30:57 +0200

    gjort klart mobil-versionen av faq-sektionen

--- script.js
+++ script.js
@@ -49,3 +49,23 @@ if(hasSetDarkmode == null) {
     darkmodeSwitch.checked = false
     document.documentElement.classList.remove('dark')
   }
+
+/*Denna kod kommer från W3 Schools Länk: https://www.w3schools.com/howto/howto_js_accordion.asp */
+var acc = document.getElementsByClassName("faq");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
